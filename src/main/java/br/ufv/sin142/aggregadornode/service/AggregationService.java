373b476014f8169f8c7a399814b23afbb41c481c
Dev-Russo <Murilorusso10@gmail.com>
2025-06-23 16:24:17 -0300

    Adicionado o import

--- src/main/java/br/ufv/sin142/aggregadornode/service/AggregationService.java
+++ src/main/java/br/ufv/sin142/aggregadornode/service/AggregationService.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.CacheEvict;
 
 import java.util.ArrayList;
 import java.util.List;
